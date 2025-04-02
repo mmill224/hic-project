ace14b2fa6c255a396bc7c1f7a75ca34209dcafb
mattmiller1996 <mkmiller96@gmail.com>
2025-04-01 22:57:43 -0400

    Further improvements to modal
    1. include size choice
    2. use custom miniButton component for close x
    3. use onMount and destroy for setting and unsetting event listeners
    4. wrap Modal usages in {#if}s to mount and unmount them at expected times

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -2,19 +2,35 @@
 	// ---------
 	// Modals are little windows that pop up on top of other content.
 	// Intended to be used as short forms or warnings.
-	// Can be used to replace js default alert if we do this more thoughtfully
+	// Can be used to replace js default alert if we do this more thoughtfully.
 	// Perfect for the little edit screen for now, though.
+	//
+	// Recommended usage: make a $state variable modalOpen in the parent component,
+	// bind it to this component's open prop and surround the modal with:
+	// 	{#if modalOpen}
+	// 		<Modal bind:open={modalOpen}>
+	// 			<!-- modal content -->
+	// 		</Modal>
+	// 	{/if}
 	// ---------
+
+	import { onMount, onDestroy } from "svelte";
+	import MiniButton from "./MiniButton.svelte";
+	import { X } from "lucide-svelte";
 
 	let {
 		children,
 		open = $bindable(false),
 		title = "",
+		size = "lg",
 	} = $props<{
 		children?: any;
 		open: boolean;
 		title: string;
+		size: "sm" | "md" | "lg" | "full";
 	}>();
+
+	var sizeClass = $state("");
 
 	const handleEscape = (event: KeyboardEvent) => {
 		if (event.key === "Escape") {
@@ -29,35 +45,50 @@
 		}
 	};
 
-	$effect(() => {
-		if (open) {
-			document.addEventListener("keydown", handleEscape);
-			// without any waiting, clicking a button to open the modal results in this event firing
-			// immediately, causing the modal to close. add a short delay so that doesn't happen.
-			setTimeout(() => {
-				document.addEventListener("click", handleClickOutside);
-			}, 100);
-		} else {
-			document.removeEventListener("keydown", handleEscape);
-			document.removeEventListener("click", handleClickOutside);
+	onMount(() => {
+		document.addEventListener("keydown", handleEscape);
+		// without any waiting, clicking a button to open the modal results in this event firing
+		// immediately, causing the modal to close. add a short delay so that doesn't happen.
+		setTimeout(() => {
+			document.addEventListener("click", handleClickOutside);
+		}, 100);
+
+		switch (size) {
+			case "sm":
+				sizeClass = "w-150 m-2 rounded-lg";
+				break;
+			case "md":
+				sizeClass = "w-200 m-2 rounded-lg";
+				break;
+			case "lg":
+				sizeClass = "w-300 m-2 rounded-lg";
+				break;
+			case "full":
+				sizeClass = "w-full h-full";
+				break;
 		}
+	});
+
+	onDestroy(() => {
+		document.removeEventListener("keydown", handleEscape);
+		document.removeEventListener("click", handleClickOutside);
 	});
 </script>
 
 {#if open}
 	<div
-		class="fixed inset-0 flex items-center justify-center"
+		class="fixed inset-0 flex items-center justify-center z-100"
 		style="background-color: rgba(0,0,0,.5)"
 	>
-		<!-- Modal content -->
-		<div
-			class="w-full max-w-sm rounded-lg bg-gray-600 p-6 shadow-lg"
-			id="modalContainer"
-		>
-			<div class="mb-4 flex justify-between text-xl font-semibold">
+		<div class="{sizeClass} bg-gray-700 p-6 shadow-lg" id="modalContainer">
+			<div
+				class="mb-4 items-center flex justify-between text-xl font-semibold"
+			>
 				<h2 class="">{title}</h2>
-				<button onclick={() => (open = !open)} class="cursor-pointer"
-					>&times;</button
+				<MiniButton
+					classes="hover:bg-gray-600"
+					color="white"
+					onclick={() => (open = !open)}><X /></MiniButton
 				>
 			</div>
 			{@render children()}
