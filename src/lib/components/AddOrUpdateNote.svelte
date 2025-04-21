a203c0045eb4864c2677ad925f1ca6c11c962bcc
nboj <60493251+nboj@users.noreply.github.com>
2025-04-21 11:10:32 -0400

    using newer modal without the #if wrapper

--- src/lib/components/AddOrUpdateNote.svelte
+++ src/lib/components/AddOrUpdateNote.svelte
@@ -3,9 +3,9 @@
 	import Button from "./Button.svelte";
 	import type { Note } from "$lib/db";
 	import { addOrUpdateNote } from "$lib/dbDal";
-	import Modal from "./Modal.svelte"; 
+	import Modal from "./Modal.svelte";
 	import TipTap from "./TipTap.svelte";
-	import { Editor } from "@tiptap/core"; 
+	import { Editor } from "@tiptap/core";
 
 	const DEFAULT_NOTE: Note = {
 		title: "",
@@ -26,7 +26,7 @@
 	let note = $state(_note);
 
 	let success = $state(false);
-	let errorMessage = $state<string>(""); 
+	let errorMessage = $state<string>("");
 	let editor = $state<Editor>();
 
 	async function handleSubmit(e?: MouseEvent) {
@@ -38,47 +38,45 @@
 		if (!note.title) {
 			errorMessage = "Please add a title to your note";
 			return;
-		} 
+		}
 		note.content = editor?.getHTML() ?? note.content;
 		success = await addOrUpdateNote({ ...note });
 		if (success) {
 			open = false;
 		}
-		onupdate && onupdate({ ...note }); 
+		onupdate && onupdate({ ...note });
 	}
 </script>
 
-{#if open}
-	<Modal size="sm" title={note.id ? "Edit Note" : "New Note"} bind:open>
-		<TextInput label="Title" bind:value={note.title} /> 
-		<TipTap
-			content={note.content ?? ""}
-			onupdate={() => handleSubmit}
-			bind:editor
-		/> 
+<Modal size="sm" title={note.id ? "Edit Note" : "New Note"} bind:open>
+	<TextInput label="Title" bind:value={note.title} />
+	<TipTap
+		content={note.content ?? ""}
+		onupdate={() => handleSubmit}
+		bind:editor
+	/>
 
-		{#if errorMessage}
-			<div class="text-red-500 m-2">{errorMessage}</div>
-		{/if}
+	{#if errorMessage}
+		<div class="text-red-500 m-2">{errorMessage}</div>
+	{/if}
 
-		<div class="flex justify-between">
-			<div class="flex">
-				<input
-					oninput={(e) => {
-						note.dueDate = new Date();
-						const tempDate = e.currentTarget.value.split("-");
-						note.dueDate.setFullYear(parseInt(tempDate[0]));
-						note.dueDate.setMonth(parseInt(tempDate[1]) - 1);
-						note.dueDate.setDate(parseInt(tempDate[2]));
-					}}
-					value={note.dueDate?.toISOString().split("T")[0]}
-					type="date"
-					id="date"
-					class="rounded-lg border border-gray-300 bg-gray-800 text-gray-100 focus:border-transparent focus:ring-2 focus:ring-blue-500 focus:outline-none"
-				/>
-			</div>
-
-			<Button onclick={handleSubmit}>Submit</Button>
+	<div class="flex justify-between">
+		<div class="flex">
+			<input
+				oninput={(e) => {
+					note.dueDate = new Date();
+					const tempDate = e.currentTarget.value.split("-");
+					note.dueDate.setFullYear(parseInt(tempDate[0]));
+					note.dueDate.setMonth(parseInt(tempDate[1]) - 1);
+					note.dueDate.setDate(parseInt(tempDate[2]));
+				}}
+				value={note.dueDate?.toISOString().split("T")[0]}
+				type="date"
+				id="date"
+				class="rounded-lg border border-gray-300 bg-gray-800 text-gray-100 focus:border-transparent focus:ring-2 focus:ring-blue-500 focus:outline-none"
+			/>
 		</div>
-	</Modal>
-{/if}
+
+		<Button onclick={handleSubmit}>Submit</Button>
+	</div>
+</Modal>
