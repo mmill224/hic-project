944554a39f93f175b7e07f32d3267523a40ab50a
Matthew Miller <mmill224@kent.edu>
2025-04-29 13:59:41 -0400

    componetize tags

---
+++ src/lib/components/TagList.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import MiniButton from "./MiniButton.svelte";
+    import { Plus } from "lucide-svelte";
+    import TagInput from "./TagInput.svelte";
+
+    let { tags = $bindable([]), editable = true } = $props<{
+        tags: string[];
+        editable?: boolean;
+    }>();
+</script>
+
+<div class="flex justify-between">
+    <MiniButton
+        color="blue"
+        onclick={() => {
+            tags = ["", ...tags];
+        }}><Plus /></MiniButton
+    >
+    {#each tags as element, i}
+        <TagInput bind:tag={tags[i]}></TagInput>
+    {/each}
+</div>
