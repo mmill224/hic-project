5d7adc5c3b1856757064baa64ada9ebc85767d1b
mattmiller1996 <mkmiller96@gmail.com>
2025-04-29 21:40:34 -0400

    Add dates to viewnote modal
    also get rid of tags tags title

--- src/lib/components/ViewNote.svelte
+++ src/lib/components/ViewNote.svelte
@@ -9,17 +9,51 @@
         tags = [],
         open = $bindable(false),
     } = $props<{ note: Note; tags?: string[]; open: boolean }>();
+
+    let dueDateString = $state("");
+    let createdDateString = $state("");
+
+    $effect(() => {
+        if (note?.dueDate) {
+            dueDateString =
+                "Due: " +
+                new Date(note.dueDate).toLocaleDateString("en-US", {
+                    month: "numeric",
+                    day: "numeric",
+                    year: "numeric",
+                });
+        }
+
+        if (note?.createdDate) {
+            createdDateString =
+                "Created: " +
+                note.createdDate.toLocaleDateString("en-US", {
+                    month: "numeric",
+                    day: "numeric",
+                    year: "numeric",
+                });
+        }
+    });
 </script>
 
 <Modal size="lg" title="View Note" bind:open>
-    <div class="content-center p-4 rounded-lg max-h-200 overflow-auto bg-gray-600">
-        <h2 class="text-xl font-bold mb-2 text-left">{note.title}</h2>
+    <div
+        class="content-center p-4 rounded-lg max-h-200 overflow-auto bg-gray-600"
+    >
+    <h2 class="text-xl font-bold mb-2 text-left">{note.title}</h2>
+    <div class="text-left">
+        <p class="text-sm text-gray-200 mb-2">
+            {createdDateString}
+        </p>
+        <p class="text-sm text-gray-200 mb-2">
+            {dueDateString}
+        </p>
+    </div>
         <TipTap
             content={note.content}
             editable={false}
             class="note-content rounded-lg text-left overflow-auto w-full overflow-hidden"
         />
     </div>
-    <h3 class="text-lg font-bold mt-2 text-left">Tags</h3>
     <TagList bind:tags editable={false} />
 </Modal>
